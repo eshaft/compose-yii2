659b0d81a61ef4cac856984b47ab5b3a1cfaea69
eshaft <eshaft@gmail.com>
2017-08-27 16:36:16 +0300

    install yii

---
+++ www/yii2sphinx.local/advanced/common/config/main.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'urlManager' => [
+            'class' => 'yii\web\UrlManager',
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+    ],
+];
